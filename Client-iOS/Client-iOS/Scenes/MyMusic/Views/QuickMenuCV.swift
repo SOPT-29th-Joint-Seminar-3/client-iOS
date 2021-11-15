22bcda167d80d48605327101c4213d47268c02c9
yangsubinn <cindy010670@gmail.com>
2021-11-15 16:18:07 +0900

    Add: QuickMenuCV 파일 추가(#2)

---
+++ Client-iOS/Client-iOS/Scenes/MyMusic/Views/QuickMenuCV.swift
@@ -0,0 +1,20 @@
+//
+//  QuickMenuCV.swift
+//  Client-iOS
+//
+//  Created by 양수빈 on 2021/11/15.
+//
+
+import UIKit
+
+class QuickMenuCV: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
