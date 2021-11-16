7098b9b66b1e74df4303a34402cfb7aafc771165
yangsubinn <cindy010670@gmail.com>
2021-11-16 23:44:12 +0900

    Add: AlbumCoverCVC 파일 추가 및 레이아웃 구현(#7)

---
+++ Client-iOS/Client-iOS/Scenes/PlaylistDetail/Views/AlbumCoverCVC.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumCoverCVC.swift
+//  Client-iOS
+//
+//  Created by 양수빈 on 2021/11/16.
+//
+
+import UIKit
+
+class AlbumCoverCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
