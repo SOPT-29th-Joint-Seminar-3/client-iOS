5c62b4164697050c3f6c7a755421b157ebd362c2
Jungbin Jung <lollin0@naver.com>
2021-11-16 15:34:57 +0900

    Add: MyPlayListHeaderView.swift 파일 생성

---
+++ Client-iOS/Client-iOS/Scenes/MyMusic/Views/MyPlayListHeaderView.swift
@@ -0,0 +1,16 @@
+//
+//  MyPlayListHeaderView.swift
+//  Client-iOS
+//
+//  Created by 1v1 on 2021/11/16.
+//
+
+import UIKit
+
+class MyPlayListHeaderView: UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
