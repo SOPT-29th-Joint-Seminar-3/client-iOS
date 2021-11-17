fd50d4fe91c4403b6ebbdd186fb7c2d7ead13055
yangsubinn <cindy010670@gmail.com>
2021-11-17 20:50:45 +0900

    Add: AlbumCoverCVC 에셋 추가 및 적용(#7)

--- Client-iOS/Client-iOS/Scenes/PlaylistDetail/Views/AlbumCoverCVC.swift
+++ Client-iOS/Client-iOS/Scenes/PlaylistDetail/Views/AlbumCoverCVC.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class AlbumCoverCVC: UICollectionViewCell {
 
+    @IBOutlet weak var centerImageView: UIImageView!
+    @IBOutlet weak var rightSecondImageView: UIImageView!
+    @IBOutlet weak var rightThirdImageView: UIImageView!
+    @IBOutlet weak var leftSecondImageView: UIImageView!
+    @IBOutlet weak var leftThirdImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBAction func touchUpBackButton(_ sender: Any) {
+        /// pop
+    }
 }
