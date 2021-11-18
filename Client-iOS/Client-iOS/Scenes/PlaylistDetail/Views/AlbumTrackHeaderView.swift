7ced41b3aec39e274a60f0afedc918b3fb78b43f
Jungbin Jung <lollin0@naver.com>
2021-11-19 01:52:22 +0900

    Add: AlbumTrackHeaderView.swift 생성

---
+++ Client-iOS/Client-iOS/Scenes/PlaylistDetail/Views/AlbumTrackHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumTrackHeaderView.swift
+//  Client-iOS
+//
+//  Created by 1v1 on 2021/11/18.
+//
+
+import UIKit
+
+class AlbumTrackHeaderView: UIView {
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setUpUI() {
+        
+    }
+}
