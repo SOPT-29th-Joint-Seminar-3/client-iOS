572cabeb94e5fa2be532d06803d8063cacf41501
김태현 <taehyk528@gmail.com>
2021-11-18 17:01:53 +0900

    Add: 탭바 연결 (#3)

--- Client-iOS/Client-iOS/Scenes/TabBar/MainTabBarController.swift
+++ Client-iOS/Client-iOS/Scenes/TabBar/MainTabBarController.swift
@@ -5,4 +5,34 @@
 //  Created by taehy.k on 2021/11/13.
 //
 
-import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        setTabBar()
+    }
+    
+    private func style() {
+        tabBar.tintColor = UIColor(color: .grey1)
+        tabBar.barTintColor = UIColor(color: .white1)
+    }
+    
+    private func setTabBar() {
+        let viewControllers = [
+            PreparingVC(),
+            PreparingVC(),
+            PreparingVC(),
+            MyMusicVC(),
+            PreparingVC(),
+        ]
+        
+        MainTab.allCases.forEach {
+            viewControllers[$0.rawValue].tabBarItem = $0.asTabBarItem()
+        }
+        
+        setViewControllers(viewControllers, animated: true)
+    }
+}
---
+++ Client-iOS/Client-iOS/Scenes/TabBar/PreparingVC.swift
@@ -0,0 +1,16 @@
+//
+//  PreparingVC.swift
+//  Client-iOS
+//
+//  Created by taehy.k on 2021/11/18.
+//
+
+import UIKit
+
+// MyMusicVC(내 음악 뷰)를 제외한 나머지 탭에 연결되는 뷰 컨트롤러
+final class PreparingVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
