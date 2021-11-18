2a3b2e4990cec9082d73f767c7f0dc63bfed918e
김태현 <taehyk528@gmail.com>
2021-11-18 16:26:11 +0900

    Chore: Image 상수 String에서 UIImage로 수정 (#3)

--- Client-iOS/Client-iOS/Common/Image.swift
+++ Client-iOS/Client-iOS/Common/Image.swift
@@ -9,14 +9,14 @@ import UIKit
 
 enum Image {
     // Top navigation
-    static let icnVoucher = "icn_voucher"
-    static let icnProfile = "icn_profile"
+    static let icnVoucher = UIImage(named: "icn_voucher")
+    static let icnProfile = UIImage(named: "icn_profile")
     
     // Bottom navigation
-    static let nvHome = "nv_home"
-    static let nvForyou = "nv_foryou"
-    static let nvSearch = "nv_search"
-    static let nvMymusic = "nv_mymusic"
-    static let nvMenu = "nv_menu"
-    static let nvMenuBadge = "nv_menu_badge"
+    static let nvHome = UIImage(named: "nv_home")
+    static let nvForyou = UIImage(named: "nv_foryou")
+    static let nvSearch = UIImage(named: "nv_search")
+    static let nvMymusic = UIImage(named: "nv_mymusic")
+    static let nvMenu = UIImage(named: "nv_menu")
+    static let nvMenuBadge = UIImage(named: "nv_menu_badge")
 }
