440383342621618e7e525a7b4b27a2f44a2b947f
Jungbin Jung <lollin0@naver.com>
2021-11-19 10:24:55 +0900

    Add: AlbumTrackTVC.swift, .xib 생성

---
+++ Client-iOS/Client-iOS/Scenes/PlaylistDetail/Views/AlbumTrackTVC.swift
@@ -0,0 +1,16 @@
+//
+//  AlbumTrackTVC.swift
+//  Client-iOS
+//
+//  Created by 1v1 on 2021/11/19.
+//
+
+import UIKit
+
+class AlbumTrackTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
