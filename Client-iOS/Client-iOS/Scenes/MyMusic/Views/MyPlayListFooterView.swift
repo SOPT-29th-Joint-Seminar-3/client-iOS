4f3a60830972d2af6f48d34489289c377bb684e1
김태현 <taehyk528@gmail.com>
2021-11-26 16:10:14 +0900

    Add: MyPlayListFooterView - ReusableView - 접기, 펼치기 버튼 셀

---
+++ Client-iOS/Client-iOS/Scenes/MyMusic/Views/MyPlayListFooterView.swift
@@ -0,0 +1,16 @@
+//
+//  MyPlayListFooterView.swift
+//  Client-iOS
+//
+//  Created by taehy.k on 2021/11/26.
+//
+
+import UIKit
+
+class MyPlayListFooterView: UICollectionReusableView {
+    @IBOutlet weak var toggleBtn: UIButton!
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
