817a21232ec7f32e4d4b3101b50f924980dbf05e
Jungbin Jung <lollin0@naver.com>
2021-11-27 22:16:15 +0900

    Add: APIConstants 파일 추가
    * baseURL 추가
    * getMyPlayListURL 추가

---
+++ Client-iOS/Client-iOS/Network/APIConstants.swift
@@ -0,0 +1,14 @@
+//
+//  APIConstants.swift
+//  Client-iOS
+//
+//  Created by 1v1 on 2021/11/27.
+//
+
+struct APIConstants {
+    // MARK: Base URL
+    static let baseURL = "https://asia-northeast3-wesopt29-328c5.cloudfunctions.net/api"
+    
+    // MARK: Feature URL
+    static let getMyPlayListURL = baseURL + "/user/"
+}
