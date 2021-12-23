01ac5002a04d406ad1f8affe953572e29dcf4c28
yangsubinn <cindy010670@gmail.com>
2021-12-23 15:19:15 +0900

    Feat: 상단뷰 레이아웃(#38)

--- Client-iOS/Client-iOS/Scenes/FoyYou/ForYouVC.swift
+++ Client-iOS/Client-iOS/Scenes/FoyYou/ForYouVC.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class ForYouVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    
+    // MARK: - IBOutlets
+    
+    // MARK: - Properties
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setStyle()
     }
-    */
-
+    
+    // MARK: - Setup
+    
+    private func setStyle() {
+        navigationController?.navigationBar.isHidden = true
+    }
 }
