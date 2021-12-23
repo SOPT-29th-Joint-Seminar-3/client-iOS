efba50bfc6d142a7b88ff486b54f776553bf850f
yangsubinn <cindy010670@gmail.com>
2021-12-23 14:56:26 +0900

    Add: ForYouVC 파일, 폴더 생성(#38)

---
+++ Client-iOS/Client-iOS/Scenes/FoyYou/ForYouVC.swift
@@ -0,0 +1,29 @@
+//
+//  ForYouVC.swift
+//  Client-iOS
+//
+//  Created by 양수빈 on 2021/12/23.
+//
+
+import UIKit
+
+class ForYouVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
