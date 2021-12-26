615445203f5318657a3642898652b181eb7b3aab
yangsubinn <cindy010670@gmail.com>
2021-12-26 14:54:54 +0900

    Add: NowCVC 추가 및 레이아웃 구현(#37)

---
+++ Client-iOS/Client-iOS/Scenes/FoyYou/Views/Cells/NowCVC.swift
@@ -0,0 +1,21 @@
+//
+//  NowCVC.swift
+//  Client-iOS
+//
+//  Created by 양수빈 on 2021/12/26.
+//
+
+import UIKit
+
+class NowCVC: UICollectionViewCell {
+
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
